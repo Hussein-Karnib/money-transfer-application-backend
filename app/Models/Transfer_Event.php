ff10ef27a54a8a6476815236896fc0254095bc68
Mohamad Chami <mohamadelchami584@gmail.com>
2025-12-01 17:27:10 +0200

    Second commit after merge(chami)

--- app/Models/Transfer_Event.php
+++ app/Models/Transfer_Event.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transfer_Event extends Model
 {
+    // Explicitly set table name to match migration
+    protected $table = 'transfer_events';
+    
     protected $fillable = [
         'transfer_id','status','note','actor_type','actor_id',
     ];
