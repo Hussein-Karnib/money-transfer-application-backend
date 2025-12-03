312d6c9c7e74877d84bcc32377fdf451204094ed
Mohamad Chami <mohamadelchami584@gmail.com>
2025-12-03 21:22:15 +0200

    Promotions are now working plus a seeder for Promotions is here

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,6 +37,7 @@ class DatabaseSeeder extends Seeder
             CurrencySeeder::class,
             CountrySeeder::class,
             TransferMethodSeeder::class,
+            PromotionSeeder::class,
         ]);
 
     }
